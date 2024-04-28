65b3e5bd6ba81ec705debbe26072d3675b06b377
KhalidJGeo <khalidjohar@geotab.com>
2024-04-28 17:11:15 -0400

    fix can bus

--- CANtest.py
+++ CANtest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
 # simple_rx_test.py
-# 
+#
 # This is simple CAN receive python program. All messages received are printed out on screen.
 # For use with PiCAN boards on the Raspberry Pi
 # http://skpang.co.uk/catalog/pican2-canbus-board-for-raspberry-pi-2-p-1475.html
@@ -20,30 +20,32 @@ import os
 
 print('\n\rCAN Rx test')
 print('Bring up CAN0....')
-os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
-time.sleep(0.1)	
+# os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+time.sleep(0.1)
 
 try:
-	bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+    # bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+    bus = can.interface.Bus(channel="can0", bustype='slcan', bitrate=500000)
 except OSError:
-	print('Cannot find PiCAN board.')
-	exit()
-	
+    print('Cannot find PiCAN board.')
+    exit()
+
 print('Ready')
 
 try:
-	while True:
-		message = bus.recv()	# Wait until a message is received.
-		
-		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-		s=''
-		for i in range(message.dlc ):
-			s +=  '{0:x} '.format(message.data[i])
-			
-		print(' {}'.format(c+s))
-	
-	
+    while True:
+        message = bus.recv()  # Wait until a message is received.
+
+        c = '{0:f} {1:x} {2:x} '.format(
+            message.timestamp, message.arbitration_id, message.dlc)
+        s = ''
+        for i in range(message.dlc):
+            s += '{0:x} '.format(message.data[i])
+
+        print(' {}'.format(c+s))
+
+
 except KeyboardInterrupt:
-	#Catch keyboard interrupt
-	os.system("sudo /sbin/ip link set can0 down")
-	print('\n\rKeyboard interrtupt')	
+    # Catch keyboard interrupt
+    os.system("sudo /sbin/ip link set can0 down")
+    print('\n\rKeyboard interrtupt')
