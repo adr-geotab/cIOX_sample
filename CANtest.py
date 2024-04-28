53fc287b5b97f88edaf734cdeccd41f6fe8e6bf6
KhalidJGeo <khalidjohar@geotab.com>
2024-04-28 17:35:24 -0400

    fix output

--- CANtest.py
+++ CANtest.py
@@ -20,14 +20,11 @@ import os
 
 print('\n\rCAN Rx test')
 print('Bring up CAN0....')
-# os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 time.sleep(0.1)
 
 try:
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
-    # bus = can.Bus(channel=None, interface=None,
-    #               config_context=None, ignore_config=False)
-    # bus = can.interface.Bus(channel="can0")
 except OSError:
     print('Cannot find PiCAN board.')
     exit()
@@ -37,14 +34,15 @@ print('Ready')
 try:
     while True:
         message = bus.recv()  # Wait until a message is received.
+        print(message.timestamp, message.arbitration_id, message.dlc, '\n')
 
-        c = '{0:f} {1:x} {2:x} '.format(
-            message.timestamp, message.arbitration_id, message.dlc)
-        s = ''
-        for i in range(message.dlc):
-            s += '{0:x} '.format(message.data[i])
+        # c = '{0:f} {1:x} {2:x} '.format(
+        #     message.timestamp, message.arbitration_id, message.dlc)
+        # s = ''
+        # for i in range(message.dlc):
+        #     s += '{0:x} '.format(message.data[i])
 
-        print(' {}'.format(c+s))
+        # print(' {}'.format(c+s))
 
 
 except KeyboardInterrupt:
