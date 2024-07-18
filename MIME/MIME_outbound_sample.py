1a752c5bd482ba40b34ad67026364864920c8684
adr-geotab <andrewde@geotab.com>
2024-07-18 16:16:32 -0400

    Add centralized msg classification process

--- MIME/MIME_outbound_sample.py
+++ MIME/MIME_outbound_sample.py
@@ -33,13 +33,11 @@ for msg in [list(_) for _ in grouped_payloads]:
 print('01 00 01\n')
 
 # Initialization and CAN startup
+poll_count, mime_index, tx_ack_index = 0, 0, 0
+outbound_msg, prev_outbound_msg = None, None
 print('Bringing up CAN0...')
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 time.sleep(0.1)
-poll_count = 0
-mime_index = 0
-tx_ack_index = 0
-outbound_msg = None
 try:
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
 except OSError:
@@ -54,16 +52,17 @@ print('--------- | -------------------------- | ---------- | --- | -------------
 try:
     while True:
         inbound_msg = bus.recv()  # Wait until a message is received.
-        print(f"cIOX->GO9 | {datetime.datetime.fromtimestamp(inbound_msg.timestamp)} | 0x{inbound_msg.arbitration_id:08X} | {hex(inbound_msg.dlc)} | {''.join(format(x, '02X') for x in inbound_msg.data).ljust(16, ' ')}", end=' | ')
+        print(f"GO9->cIOX | {datetime.datetime.fromtimestamp(inbound_msg.timestamp)} | 0x{inbound_msg.arbitration_id:08X} | {hex(inbound_msg.dlc)} | {''.join(format(x, '02X') for x in inbound_msg.data).ljust(16, ' ')}", end=' | ')
+        classify_inbound_msg(inbound_msg, prev_outbound_msg)
         
-        if (inbound_msg.arbitration_id == 0x00010000 and poll_count == 0): 
-            print("Poll Request")
-            outbound_msg = send_outbound_msg(bus, 0x0002ABCD, [0x01, 0x01, 0x00, 0x12, 0x16, 0x00, 0x00, 0x9A], 'Poll Response (Handshake)')
+        if (inbound_msg.arbitration_id == 0x00010000): 
+            if poll_count == 0:
+                outbound_msg = send_outbound_msg(bus, 0x0002ABCD, [0x01, 0x01, 0x00, 0x12, 0x16, 0x00, 0x00, 0x9A], 'Poll Response (Handshake)')
+            else:
+                outbound_msg = send_outbound_msg(bus, 0x0002ABCD, [0x00], 'Poll Response')
             poll_count += 1
 
         elif (inbound_msg.arbitration_id == 0x0014ABCD): 
-            print(f'Acknowledgement of 0x{prev_outbound_msg.arbitration_id:08X}')
-
             if poll_count == 2:
                 outbound_msg = send_outbound_msg(bus, 0x001DABCD, [0x01, 0x01, 0x70, 0x10, 0x01, 0x00], 'Send External Device ID')
                 poll_count += 1
@@ -81,54 +80,13 @@ try:
                 outbound_msg = send_outbound_msg(bus, 0x0025ABCD, [0x01, 0x00, 0x01], f'MIME-{mime_index} (Ending Packet Wrapper)')
                 mime_index += 1
 
-        elif (inbound_msg.arbitration_id == 0x00010000 and poll_count > 0):
-            poll_count +=1
-            print('Poll Request')
-            outbound_msg = send_outbound_msg(bus, 0x0002ABCD, [0x00], 'Poll Response')
-
-        elif inbound_msg.arbitration_id == 0x260000:
-            print('GO Status Information Log', end='')
-            if inbound_msg.data[0] == 0x00 and inbound_msg.data[1] == 0x00:
-                if inbound_msg.data[2] == 0x00:
-                    print(' (Ignition off)')
-                elif inbound_msg.data[2] == 0x01:
-                    print(' (Ignition on)')
-            elif inbound_msg.data[0] == 0x01 and inbound_msg.data[1] == 0x00:
-                if inbound_msg.data[2] == 0x00:
-                    print(' (Modem is not ready)')
-                elif inbound_msg.data[2] == 0x01:
-                    print(' (Modem is available)')
-            else:
-                print()
-
-        elif inbound_msg.arbitration_id == 0x040000:
-            print('Wakeup')
-
-        elif inbound_msg.arbitration_id == 0x1CABCD and inbound_msg.data[0] == 0x00:
-            print('GO Accept Message to Buffer', end=' ')
-            if inbound_msg.data[1] == 0x00:
-                print('(Failed)')
-                print(f"\033[93mWARNING: Modem transmission failed. This typically indicates that it is not connected. The MIME content was not transferred.\033[0m")
-            elif inbound_msg.data[1] == 0x01:
-                print('(Success)')
-
-        elif inbound_msg.arbitration_id == 0x1CABCD and inbound_msg.data[0] == 0x05:
-            if inbound_msg.data[1] == 0x00:
-                print('External Device Channel Disabled')
-            elif inbound_msg.data[1] == 0x01:
-                print('External Device Channel Enabled')
+        elif inbound_msg.arbitration_id == 0x1CABCD and [inbound_msg.data[0], inbound_msg.data[1]] == [0x00, 0x00]:
+            print(f"\033[93mWARNING: Modem transmission failed. This typically indicates that it is not connected. The MIME content was not transferred.\033[0m")
 
         elif inbound_msg.arbitration_id == 0x0BABCD:
-            print('TX Data')
             tx_ack_index += 1
             if tx_ack_index == 2:
                 print(f"\033[92mSUCCESS! The MyGeotab database has received the MIME message and it can be pulled via API.\033[0m")
-    
-        elif inbound_msg.arbitration_id == 0x260000:
-            print('GO Status Information Log')
-
-        else:
-            print('Unclassified Message')
 
         prev_outbound_msg = outbound_msg
 
--- MIME/functions.py
+++ MIME/functions.py
@@ -1,11 +1,60 @@
 import can
 import time
 import datetime
-import os
 
-# Send 
+# Construct and send CAN message
 def send_outbound_msg(bus, arb_id, data, desc):
     outbound_msg = can.Message(arbitration_id=arb_id, data=data, timestamp=time.time())
     print(f"cIOX->GO9 | {datetime.datetime.fromtimestamp(outbound_msg.timestamp)} | 0x{arb_id:08X} | {hex(len(data))} | {''.join(format(x, '02X') for x in data).ljust(16, ' ')} | {desc}")
     bus.send(outbound_msg)
+
     return outbound_msg
+
+def classify_inbound_msg(inbound_msg, prev_outbound_msg):
+    if (inbound_msg.arbitration_id == 0x00010000): 
+        print("Poll Request")
+    
+    elif (inbound_msg.arbitration_id == 0x0014ABCD): 
+        print(f'Acknowledgement of 0x{prev_outbound_msg.arbitration_id:08X}')
+
+    elif inbound_msg.arbitration_id == 0x260000:
+        print('GO Status Information Log', end='')
+        if inbound_msg.data[0] == 0x00 and inbound_msg.data[1] == 0x00:
+            if inbound_msg.data[2] == 0x00:
+                print(' (Ignition off)')
+            elif inbound_msg.data[2] == 0x01:
+                print(' (Ignition on)')
+        elif inbound_msg.data[0] == 0x01 and inbound_msg.data[1] == 0x00:
+            if inbound_msg.data[2] == 0x00:
+                print(' (Modem is not ready)')
+            elif inbound_msg.data[2] == 0x01:
+                print(' (Modem is available)')
+        else:
+            print()
+
+    elif inbound_msg.arbitration_id == 0x040000:
+        print('Wakeup')
+
+    elif inbound_msg.arbitration_id == 0x1CABCD and inbound_msg.data[0] == 0x00:
+        print('GO Accept Message to Buffer', end=' ')
+        if inbound_msg.data[1] == 0x00:
+            print('(Failed)')
+        elif inbound_msg.data[1] == 0x01:
+            print('(Success)')
+
+    elif inbound_msg.arbitration_id == 0x1CABCD and inbound_msg.data[0] == 0x05:
+        if inbound_msg.data[1] == 0x00:
+            print('External Device Channel Disabled')
+        elif inbound_msg.data[1] == 0x01:
+            print('External Device Channel Enabled')
+
+    elif inbound_msg.arbitration_id == 0x0BABCD:
+        print('TX Data')
+
+    elif inbound_msg.arbitration_id == 0x260000:
+        print('GO Status Information Log')
+
+    else:
+        print('Unclassified Message')
+    
+    return
